ed561a5acb0eb398c9ae1832012f0905e5af742d
Shreshth Khilani <shreshth.khilani@gmail.com>
2019-02-15 01:14:52 -0500

    Rename routes

--- api/dinners/index.go
+++ api/dinners/index.go
@@ -1,4 +1,4 @@
-package dinners
+package main
 
 import (
 	"context"
--- api/main.go
+++ api/main.go
@@ -7,6 +7,6 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/dinners", dinners.Handler)
+	http.HandleFunc("/api/dinners", dinners.Handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
